e8af67158afa84a8632ee8ba691509237540e6af
Gmarket-joopark <107295585+Gmarket-joopark@users.noreply.github.com>
2022-08-23 16:52:37 +0900

    init

---
+++ TAU-python2022/02Variables.py
@@ -0,0 +1,25 @@
+CARS = 25
+cars = 26
+flost_cars = 34.66
+number_of_cars = 23
+kind_of_cars = "Benz"
+print(number_of_cars,kind_of_cars, CARS, cars)
+number_of_cars = number_of_cars+cars+flost_cars
+"""
+This is a multi-line comment
+You can use this kind of comment to
+make longer notes as you are learning.
+In python, these are often used as
+print(number_of_cars)
+docstrings.
+"""
+kind_of_cars = kind_of_cars +" BMW"
+print(number_of_cars,kind_of_cars, CARS, cars)
+
+
+name = "Janelle Jones"
+type_of_car = "Rolls Royce"
+school = "Foundation Elementary School"
+
+print(name + " works at "+school)
+print("{} works at {}.".format(name,school))
---
+++ TAU-python2022/03Function.py
@@ -0,0 +1,17 @@
+def add_1():
+    a = 1
+    b = 2
+    print(a+b)
+    print("a = {}, b = {} , a + b = {}".format(a,b,a+b))
+
+
+
+def add_2():
+    a = int(input("enter a number"))
+    b = int(input("enter b number"))
+    print(a+b)
+    print("a = {}, b = {} , a + b = {}".format(a,b,a+b))
+
+
+add_1() #기본 함수
+add_2() #입력형  함수
