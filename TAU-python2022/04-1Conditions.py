1cc46f432741d797f974851a200904657bb94bcd
Gmarket-joopark <107295585+Gmarket-joopark@users.noreply.github.com>
2022-08-26 16:40:17 +0900

    test p

---
+++ TAU-python2022/04-1Conditions.py
@@ -0,0 +1,31 @@
+def add():
+    a = float(input("Enter a number. "))
+    b = float(input("Enter another number. "))
+    print(a + b)
+
+def subtraction():
+    a = float(input("Enter a number. "))
+    b = float(input("Enter another number. "))
+    print(a - b)
+
+def multiply():
+    a = float(input("Enter a number. "))
+    b = float(input("Enter another number. "))
+    print(a * b)
+
+def divide():
+    a = float(input("Enter a number. "))
+    b = float(input("Enter another number. "))
+    print(a / b)
+
+operation = input(" please input operator")
+if operation=='+':
+    add()
+elif operation == '-':
+    subtraction()
+elif operation == '*':
+    multiply()
+elif operation=='/':
+    divide()
+else:
+ print ("wrong op")
---
+++ TAU-python2022/04Compares.py
@@ -0,0 +1,24 @@
+print ("1>1 :",1>1)
+print ("1=1 : ",1==1)
+print ("1<1 :", 1<1)
+print ("1!=1 : ",1!=1)
+
+name= input ("your name?")
+
+print("front if")
+
+if name=='k':
+  print ("hello {}".format(name))
+
+if name=='king':
+  print ("hello {}".format(name))
+elif name=='b':
+  print ("other elif")
+elif name!='c':
+  print ("elif != stms c")
+else:
+  print("else")
+
+print("after if stms")
+
+
